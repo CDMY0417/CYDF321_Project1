8aebd6444b7d3ed0a98b6afc8f12798e3fd38c7f
CDMY0417 <tikiliki0417@naver.com>
2022-04-30 09:26:09 -0400

    Modified client and server

--- server.c
+++ server.c
@@ -29,7 +29,7 @@ int main(int argc, char* argv[])
 	uint16_t port = atoi(argv[1]);
 
 	//create socket
-	if ((sd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+	if ((sd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
 		perror("socket creation error"); //socket creation error
 		return FAIL;
 	}
