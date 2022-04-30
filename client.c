e9f9658f21cd435520494a9e7a86b98fd1f3c5d5
CDMY0417 <tikiliki0417@naver.com>
2022-04-30 08:08:50 -0400

    Added server & client

---
+++ client.c
@@ -0,0 +1,78 @@
+//chat client
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define MAXLEN 100
+#define FAIL -1
+#define SUCCESS 0
+
+void usage() {
+	printf("syntax : ./client <ip address> <port number>\n");
+	printf("sample : ./client 127.0.0.1 8080\n");
+}
+
+int main(int argc, char* argv[])
+{
+	int sd, len;
+	char buff[MAXLEN];
+	int i = 0;
+	struct sockaddr_in client_addr;
+	if (argc != 3) {
+		usage();
+		return FAIL;
+	}
+	uint16_t port = atoi(argv[2]);
+
+	//create socket
+	if ((sd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+		perror("socket creation error"); //socket creation error
+		return FAIL;
+	}
+
+	//assign IP and PORT
+	char* ip = argv[1];
+	in_addr_t ip_addr = inet_addr(ip);
+	client_addr.sin_family = AF_INET;
+	client_addr.sin_port = htons(port);
+	client_addr.sin_addr.s_addr = ip_addr;
+
+	//connect socket
+	if (connect(sd, (struct sockaddr*)&client_addr, sizeof(client_addr)) < 0) {
+		perror("socket connect error"); //socket connect error
+		return FAIL;
+	}
+	printf("Connected\n");
+
+	//chat
+	while (1) {
+		//write
+		printf("To server : ");
+		memset(buff, 0, MAXLEN);
+		i = 0;
+		while ((buff[i++] = getchar()) != '\n');
+		write(sd, buff, sizeof(buff));
+		len = strlen(buff) - 1;
+		if (strncmp("QUIT", buff, (len > 4? len : 4)) == 0) {
+			printf("Disconnected\n");
+			break;
+		}
+
+		//read
+		memset(buff, 0, MAXLEN);
+		read(sd, buff, sizeof(buff));
+		printf("From server : %s", buff);
+		len = strlen(buff) - 1;
+		if (strncmp("QUIT", buff, (len > 4? len : 4)) == 0) {
+			printf("Disconnected\n");
+			break;
+		}
+	}
+
+	//close socket
+	close(sd);
+	return SUCCESS;
+}
---
+++ server.c
@@ -0,0 +1,91 @@
+//chat server
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#define MAXLEN 100
+#define QUEUELIMIT 3
+#define FAIL -1
+#define SUCCESS 0
+
+void usage() {
+	printf("syntax : ./server <port number>\n");
+	printf("sample : ./server 8080\n");
+}
+
+int main(int argc, char* argv[])
+{
+	int sd, cd, len;
+	char buff[MAXLEN];
+	int i = 0;
+	struct sockaddr_in server_addr, client_addr;
+	if (argc != 2) {
+		usage();
+		return FAIL;
+	}
+	uint16_t port = atoi(argv[1]);
+
+	//create socket
+	if ((sd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+		perror("socket creation error"); //socket creation error
+		return FAIL;
+	}
+
+	//assign IP and PORT
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(port);
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	//bind socket
+	if (bind(sd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+		perror("socket bind error"); //socket bind error
+		return FAIL;
+	}
+
+	//listen
+	if (listen(sd, QUEUELIMIT) < 0) {
+		perror("server listen error"); //server listen error
+		return FAIL;
+	}
+	len = sizeof(client_addr);
+
+	//accept
+	if ((cd = accept(sd, (struct sockaddr*)&client_addr, &len)) < 0) {
+		perror("server accept error");
+		return FAIL;
+	}
+
+	printf("Connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+	//chat
+	while (1) {
+		//read
+		memset(buff, 0, MAXLEN);
+		read(cd, buff, sizeof(buff));
+		printf("From client : %s", buff);
+		len = strlen(buff) - 1;
+		if (strncmp("QUIT", buff, (len > 4? len: 4)) == 0) {
+			printf("Disconnected\n");
+			break;
+		}
+
+		//write
+		printf("To client : ");
+		memset(buff, 0, MAXLEN);
+		i = 0;
+		while ((buff[i++] = getchar()) != '\n');
+		write(cd, buff, sizeof(buff));
+		len = strlen(buff) - 1;
+		if (strncmp("QUIT", buff, (len > 4? len : 4)) == 0) {
+			printf("Disconnected\n");
+			break;
+		}
+	}
+
+	//close socket
+	close(sd);
+	return SUCCESS;
+}
